94b0ff44449a0f10b5a64337fdb45cccf476aff4
Ben Hess <pilotben@comcast.net>
2024-10-16 11:23:45 -0700

    Step 3 complete: added automatic clicking

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import "./style.css";
 
 const app: HTMLDivElement = document.querySelector("#app")!;
 
-let count : number = 0;
+let count: number = 0;
 
 const gameName = "My supercalifragilisticexpialidocious game";
 document.title = gameName;
@@ -18,7 +18,12 @@ app.append(counterLabel);
 const btn = document.createElement("button");
 btn.innerHTML = "🎆";
 btn.addEventListener("click", () => {
-    count++;
-    counterLabel.innerHTML = `${count} Fireworks`;
+  count++;
+  counterLabel.innerHTML = `${count} Fireworks`;
 });
 app.append(btn);
+
+setInterval(() => {
+    count++;
+    counterLabel.innerHTML = `${count} Fireworks`;
+}, 1000);
