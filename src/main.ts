2d8eaee9e9d12cb6cf27a886c3682dfd7a4caad9
Ben Hess <pilotben@comcast.net>
2024-10-16 11:55:30 -0700

    Step 5 complete: added upgrade perchacing

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,6 @@ import "./style.css";
 
 const app: HTMLDivElement = document.querySelector("#app")!;
 
-let count: number = 0;
-let time: number = 0;
-
 const gameName = "My supercalifragilisticexpialidocious game";
 document.title = gameName;
 
@@ -12,9 +9,21 @@ const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
 
+let count: number = 0;
 function update() {
-    counterLabel.innerHTML = `${count.toFixed(3)} Fireworks`;
+  counterLabel.innerHTML = `${count.toFixed(3)} Fireworks`;
+  upgradeBtn.disabled = count < 10;
 }
+
+let growthRate: number = 0;
+const upgradeBtn = document.createElement("button");
+upgradeBtn.innerHTML = "💥 Perchase Automatic Firework Cannon";
+upgradeBtn.addEventListener("click", () => {
+  count -= 10;
+  growthRate++;
+  update();
+});
+app.append(upgradeBtn);
 
 const counterLabel = document.createElement("div");
 app.append(counterLabel);
@@ -28,14 +37,15 @@ btn.addEventListener("click", () => {
 });
 app.append(btn);
 
+let time: number = 0;
 function tick() {
-    requestAnimationFrame(() => tick());
-    const newTime = performance.now();
-    const dt = newTime - time;
-    time = newTime;
+  requestAnimationFrame(() => tick());
+  const newTime = performance.now();
+  const dt = newTime - time;
+  time = newTime;
 
-    count += dt / 1000;
-    update();
+  count += (dt / 1000) * growthRate;
+  update();
 }
 
 tick();
