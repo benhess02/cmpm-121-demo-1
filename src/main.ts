9723b9b3c97b254660bb6b2235baa77bb6bcef26
Ben Hess <pilotben@comcast.net>
2024-10-18 11:49:56 -0700

    Step 6 complete: added mutliple upgrades and status

--- src/main.ts
+++ src/main.ts
@@ -12,21 +12,50 @@ app.append(header);
 let count: number = 0;
 function update() {
   counterLabel.innerHTML = `${count.toFixed(3)} Fireworks`;
-  upgradeBtn.disabled = count < 10;
+  growthRateLabel.innerHTML = `${calculateGrowthRate().toFixed(2)} cookies/sec`;
+  upgradesLabel.innerHTML = `Upgrades: ${upgradeACount} A, ${upgradeBCount} B, ${upgradeCCount} C`;
+  upgradeABtn.disabled = count < 10;
+  upgradeBBtn.disabled = count < 100;
+  upgradeCBtn.disabled = count < 1000;
 }
 
-let growthRate: number = 0;
-const upgradeBtn = document.createElement("button");
-upgradeBtn.innerHTML = "💥 Perchase Automatic Firework Cannon";
-upgradeBtn.addEventListener("click", () => {
+let upgradeACount : number = 0;
+let upgradeBCount : number = 0;
+let upgradeCCount : number = 0;
+
+const upgradeABtn = document.createElement("button");
+upgradeABtn.innerHTML = "Perchase Upgrade A";
+upgradeABtn.addEventListener("click", () => {
   count -= 10;
-  growthRate++;
+  upgradeACount += 1;
   update();
 });
-app.append(upgradeBtn);
+app.append(upgradeABtn);
+
+const upgradeBBtn = document.createElement("button");
+upgradeBBtn.innerHTML = "Perchase Upgrade B";
+upgradeBBtn.addEventListener("click", () => {
+  count -= 100;
+  upgradeBCount += 1;
+  update();
+});
+app.append(upgradeBBtn);
+
+const upgradeCBtn = document.createElement("button");
+upgradeCBtn.innerHTML = "Perchase Upgrade C";
+upgradeCBtn.addEventListener("click", () => {
+  count -= 1000;
+  upgradeCCount += 1;
+  update();
+});
+app.append(upgradeCBtn);
 
 const counterLabel = document.createElement("div");
 app.append(counterLabel);
+const growthRateLabel = document.createElement("div");
+app.append(growthRateLabel);
+const upgradesLabel = document.createElement("div");
+app.append(upgradesLabel);
 update();
 
 const btn = document.createElement("button");
@@ -37,6 +66,10 @@ btn.addEventListener("click", () => {
 });
 app.append(btn);
 
+function calculateGrowthRate() : number {
+  return upgradeACount * 0.1 + upgradeBCount * 2 + upgradeCCount * 50;
+}
+
 let time: number = 0;
 function tick() {
   requestAnimationFrame(() => tick());
@@ -44,7 +77,7 @@ function tick() {
   const dt = newTime - time;
   time = newTime;
 
-  count += (dt / 1000) * growthRate;
+  count += (dt / 1000) * calculateGrowthRate();
   update();
 }
 
